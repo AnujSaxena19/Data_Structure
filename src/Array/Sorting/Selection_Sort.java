1a0ca89aabb5f3e0bdd97d9930fecd8a88ae5515
AnujSaxena19 <19anujsaxena19@gmail.com>
2020-05-25 00:45:52 +0530

    Sorting

---
+++ src/Array/Sorting/Selection_Sort.java
@@ -0,0 +1,27 @@
+package Array.Sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Selection_Sort {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n-1;i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[min]) {
+                    min = j;
+                }
+            }
+            int temp = a[min];
+            a[min] = a[i];
+            a[i] = temp;
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
