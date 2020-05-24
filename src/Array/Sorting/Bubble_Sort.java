bb4fdb7528381c32871f544cc8e2acbc603fd5c3
AnujSaxena19 <19anujsaxena19@gmail.com>
2020-05-25 00:32:35 +0530

    Sorting

---
+++ src/Array/Sorting/Bubble_Sort.java
@@ -0,0 +1,26 @@
+package Array.Sorting;
+import java.util.*;
+
+public class Bubble_Sort {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int temp;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
